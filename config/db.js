f27760e5903c1ab6338001f11de2c70d0070e82a
Waqar Hussain <whussain@gtr.com.my>
2024-08-01 14:38:31 +0400

    fixes

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DEV_DATABASE_URL, {
+    await mongoose.connect(process.env.DEV_DEV_DATABASE_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
